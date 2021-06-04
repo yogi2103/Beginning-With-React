ac5ad2c50791d8192d86ef9f37b656eb7a3f14bc
Kabira Singh <yogesh.baghel86@gmail.com>
2021-06-04 15:27:32 +0530

    UseState in restless components

--- react-the-complete-guide/src/App.js
+++ react-the-complete-guide/src/App.js
@@ -1,40 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state={
+const app =(props)=>{
+  const [personsState, setPersonState] = useState({
     persons:[
       {name:'Max', age:28},
       {name:'Manu', age:29},
       {name:'Stephanie',age:26}
     ],
     otherstate:'Voila'
-  }
+  })
 
-  switchnameHandler=()=>{
-    console.log('CLicked!');
-    this.setState({
+  const switchnameHandler=()=>{
+    setPersonState({
       persons:[
-        {name:'Yogesh',age:21},
+        {name:'Yogi', age:23},
         {name:'Manu', age:29},
         {name:'Stephanie',age:26}
       ]
     })
   }
 
-  render() {
     return (
       <div className="App">
         <h1>I'm a react App</h1>
-        <button onClick={this.switchnameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>love Cricket</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        <p>{this.state.otherstate}</p>
+        <button onClick={switchnameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>love Cricket</Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        <p>{personsState.otherstate}</p>
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
