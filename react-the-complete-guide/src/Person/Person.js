16b9febbbdddee31f6e76db436e3131e81c0d751
Kabira Singh <yogesh.baghel86@gmail.com>
2021-06-06 18:03:24 +0530

    Component update life Cycle

--- react-the-complete-guide/src/Person/Person.js
+++ react-the-complete-guide/src/Person/Person.js
@@ -1,7 +1,24 @@
 import React,{Component} from 'react';
 import './Person.css'
 
+
 class Person extends Component{
+
+    shouldComponentUpdate(nextProps, nextState){
+        console.log('[Person.js] shouldComponentUpdate');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('[Person.js] getSnapshotBeforeUpdate');
+        return {message:'Snapshot!'};
+    }
+
+    componentDidUpdate(prevProps, prevState,snapshot){
+        console.log('[Person.js] componentDidUpdate');
+        console.log(snapshot);
+    }
+
     render(){
         console.log('[Person.js] rendering');
         return (
